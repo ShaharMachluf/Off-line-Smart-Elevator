08958b9d884d06dcb0ba96c6730156952dfac751
ShaharMachluf <85555432+ShaharMachluf@users.noreply.github.com>
2021-11-14 15:41:43 +0200

    Update Elevator.py

--- Ex1/src/Elevator.py
+++ Ex1/src/Elevator.py
@@ -1,49 +1,22 @@
-from multiprocessing.resource_sharer import stop
 
 
 class Elevator:
-    _id=0
-    _speed=0.0
-    _minFloor=0
-    _maxFloor=0
-    _closeTime=0.0
-    _openTime=0.0
-    _startTime=0.0
-    _stopTime=0.0
-    _calls=[]
-    def __init__(self,id,speed,minfloor,maxfloor,closetime,opentime,startime,stoptime):
-        self._id=id
-        self._speed=speed
-        self._minFloor=minfloor
-        self._maxFloor=maxfloor
-        self._closeTime=closetime
-        self._openTime=opentime
-        self._startTime=startime
-        self._stopTime=stoptime
 
-    def getid(self):
-        return self._id
+    def __init__(self, _id, speed, min_floor, max_floor, close_time, open_time, star_time, stop_time):
+        self.id = _id
+        self.speed = speed
+        self.min_floor = min_floor
+        self.max_floor = max_floor
+        self.close_time = close_time
+        self.open_time = open_time
+        self.start_time = star_time
+        self.stop_time = stop_time
+        self.calls = []
 
-    def getstopTime(self):
-        return self._stopTime
-
-    def getspeed(self):
-        return self._speed
-
-    def getminfloor(self):
-        return self._minFloor
-
-    def getmaxfloor(self):
-        return self._maxFloor
-
-    def get_closetime(self):
-        return self._closeTime
-
-    def get_openTime(self):
-        return self._openTime
-
-    def get_startTime(self):
-        return self._startTime
-
-    def addcall(self, name):
-        self._calls.append(name)
+    def add_call(self, call):
+        """
+        add call to the call list
+        :param call: a call allocated to self
+        :return: void
+        """
+        self.calls.append(call)
