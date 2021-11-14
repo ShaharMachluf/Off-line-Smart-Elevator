158d24b6bec1607c2d354dcc04ba7c31c8da67eb
ShaharMachluf <85555432+ShaharMachluf@users.noreply.github.com>
2021-11-14 15:40:41 +0200

    Update Calls.py

--- Ex1/src/Calls.py
+++ Ex1/src/Calls.py
@@ -1,15 +1,8 @@
-import csv
+class Calls:
 
-    class Calls:
-        _Time=0.0
-        _Src=0
-        _dest=0
-        _status=0
-        _elevator=-1
-
-        def __init__(self,namelist):
-            self._Time=namelist[1]
-            self._Src=namelist[2]
-            self._dest=namelist[3]
-            self._status=namelist[4]
-            self._elevator=namelist[5]
+    def __init__(self, namelist):
+        self.time = namelist[1]
+        self.src = namelist[2]
+        self.dest = namelist[3]
+        self.status = namelist[4]
+        self.elevator = namelist[5]
