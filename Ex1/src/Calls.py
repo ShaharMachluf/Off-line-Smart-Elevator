270ed1d3d381856392f0c3cb0e5a565afb6012e9
shaimoo <92544909+shaimoo@users.noreply.github.com>
2021-11-15 23:10:23 +0200

    Delete Calls.py

--- Ex1/src/Calls.py
+++
@@ -1,8 +0,0 @@
-class Call:
-
-    def __init__(self, namelist):
-        self.time = namelist[1]
-        self.src = namelist[2]
-        self.dest = namelist[3]
-        self.status = namelist[4]
-        self.elevator = namelist[5]
