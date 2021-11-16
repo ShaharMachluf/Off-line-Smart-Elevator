d874a0702310cb3e0d7dc6e9d2177a81f9f45799
ShaharMachluf <85555432+ShaharMachluf@users.noreply.github.com>
2021-11-16 14:51:26 +0200

    Update simulator.py

--- Ex1/src/simulator.py
+++ Ex1/src/simulator.py
@@ -11,38 +11,106 @@ def direction(last_floor, later_floor):
 
 
 class Simulator:
-    def __init__(self, elev: Elevator, time: float):
+    def __init__(self, elev: Elevator):
         self.elev = elev
-        self.time = time
         self.times = {}
 
     def set_pos(self, c: Call):
         src = str(c.src)
         dest = str(c.dest)
         if len(self.times) == 0:
-            self.times[src] = c.time+self.travel_time(0, c.src)
-            self.times[dest] = self.times[src] + self.travel_time(c.src, c.dest)
+            self.times[src] = c.time+self.travel_time(0, c.src) + c.time
+            self.times[dest] = self.times[src] + self.travel_time(c.src, c.dest) + self.times[src]
         else:
             dict(sorted(self.times.items(), key=lambda item: item[1]))
             it = iter(self.times)
             while it[1] < c.time and it.__next__() is not None:
-                it.__next__()
+                it = it.__next__()
             last_floor = it
             if it.__next__() is None:
-                self.times[src] = self.travel_time(int(last_floor[0]), c.src)
-                self.times[dest] = self.travel_time(c.src, c.dest)
+                self.times[src] = self.travel_time(int(last_floor[0]), c.src) + c.time
+                self.times[dest] = self.travel_time(c.src, c.dest) + self.times[src]
             else:
-                later_floor = it.__next__()
-                curr_direct = direction(last_floor, later_floor)
-                # complete the case that the call is in the middle
+                highest = self.highest_floor()
+                lowest = self.lowest_floor()
+                src_item = self.insert_src(last_floor, c, highest, lowest)
+                dict(sorted(self.times.items(), key=lambda item: item[1]))
+                last_floor = iter(self.times)    
+                while last_floor.__next__() != src_item:
+                    last_floor.__next__()
+                self.insert_dest(last_floor, c, highest, lowest)          
+
+    def insert_src(self, last_floor, c, highest, lowest):
+        if int(highest[0]) < c.src:
+            while last_floor != highest:
+                last_floor.__next__()
+            src_time = self.travel_time(int(last_floor[0]), c.src) + last_floor[1]
+            last_floor = last_floor.__next__
+        elif int(lowest[0]) > c.src:
+            while last_floor != lowest:
+                last_floor.__next__()
+            src_time = self.travel_time(int(last_floor[0]), c.src) + last_floor[1]
+            last_floor = last_floor.__next__
+        else:
+            while last_floor.__next__() is not None:
+                if int(last_floor[0]) < c.src < int(last_floor.__next__()[0]) or int(last_floor[0]) > c.src > int(
+                        last_floor.__next__()[0]):
+                    src_time = self.travel_time(int(last_floor[0]), c.src) + last_floor[1]
+                    last_floor = last_floor.__next__()
+                    break
+                last_floor = last_floor.__next__()
+        last_floor[1] = src_time + self.travel_time(c.src, int(last_floor[0]))
+        while last_floor.__next__() is not None:
+            last_floor.__next__()[1] = last_floor[1] + self.travel_time(int(last_floor[0]), int(last_floor.__next__()[0]))
+        self.times[str(c.src)] = src_time
+        return str(c.src), src_time
+
+    def insert_dest(self, last_floor, c, highest, lowest):
+        if int(highest[0]) < c.dest:
+            while last_floor != highest:
+                last_floor.__next__()
+            dest_time = self.travel_time(int(last_floor[0]), c.dest) + last_floor[1]
+            last_floor = last_floor.__next__
+        elif int(lowest[0]) > c.dest:
+            while last_floor != lowest:
+                last_floor.__next__()
+            dest_time = self.travel_time(int(last_floor[0]), c.dest) + last_floor[1]
+            last_floor = last_floor.__next__
+        else:
+            while last_floor.__next__() is not None:
+                if int(last_floor[0]) < c.dest < int(last_floor.__next__()[0]) or int(last_floor[0]) > c.dest > int(
+                        last_floor.__next__()[0]):
+                    dest_time = self.travel_time(int(last_floor[0]), c.dest) + last_floor[1]
+                    last_floor = last_floor.__next__()
+                    break
+                last_floor = last_floor.__next__()
+        last_floor[1] = dest_time + self.travel_time(c.dest, int(last_floor[0]))
+        while last_floor.__next__() is not None:
+            last_floor.__next__()[1] = last_floor[1] + self.travel_time(int(last_floor[0]),
+                                                                        int(last_floor.__next__()[0]))
+        self.times[str(c.dest)] = dest_time
+
+    def highest_floor(self):
+        max_floor = (str(self.elev.min_floor), 0.0)
+        for item in self.times.items():
+            if int(item[0]) >= max_floor:
+                max_floor = item
+        return max_floor
+
+    def lowest_floor(self):
+        min_floor = (str(self.elev.max_floor), 0.0)
+        for item in self.times.items():
+            if int(item[0]) <= min_floor:
+                min_floor = item
+        return min_floor
 
     def travel_time(self, src, dest):
         start = self.elev.start_time
         stop = self.elev.stop_time
         open_t = self.elev.open_time
         close_t = self.elev.close_time
-        dist = (src+dest)/self.elev.speed
-        return (self.stops(src, dest) * (start+stop+open_t+close_t)) + dist
+        dist = (src + dest) / self.elev.speed
+        return ((self.stops(src, dest) + 1) * (start + stop + open_t + close_t)) + dist
 
     def stops(self, src, dest):
         count = 0
