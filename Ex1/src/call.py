3c7c830c56ff620992fc8702ab10a6f3af4d6ee8
shaimoo <92544909+shaimoo@users.noreply.github.com>
2021-11-17 12:39:34 +0200

    Add files via upload

--- Ex1/src/call.py
+++ Ex1/src/call.py
@@ -1,15 +1,10 @@
-import csv
 
-    class call:
-       
-            def __init__(self, namelist):
-                try:
-                    with open(namelist, "r") as f:
-                        reader =csv.reader(f)
-                        self.time = reader[1]
-                        self.src = reader[2]
-                        self.dest = reader[3]
-                        self.status = reader[4]
-                        self.elevator = reader[5]
-                except FileNotFoundError:
-                    print("file not found")
+
+class Call:
+    def __init__(self, namelist):
+
+        self.time = namelist[1]
+        self.src = namelist[2]
+        self.dest = namelist[3]
+        self.status = namelist[4]
+        self.elevator = namelist[5]
